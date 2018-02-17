7c376d999bf6389c39ce521c079d5bdebecb519e
kkdroidgit <kartikdroid17@gmail.com>
2018-02-17 11:51:07 +0530

    Added Just Java Project

---
+++ JustJava/app/src/main/java/in/paperwrk/justjava/MainActivity.java
@@ -0,0 +1,30 @@
+package in.paperwrk.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+}
